553506eabd555b76e5e6bb3a3c2198fa87696df3
jammergreek <72779305+jammergreek@users.noreply.github.com>
2024-02-11 16:35:23 +0500

    Update ruRU.lua

--- Gnomeregan_Classic/Locales/ruRU.lua
+++ Gnomeregan_Classic/Locales/ruRU.lua
@@ -2,15 +2,15 @@ local L = BigWigs:NewBossLocale("Grubbis Discovery", "ruRU")
 if not L then return end
 if L then
 	L.bossName = "Грязнюк"
-	--L.aoe = "AoE melee damage"
-	--L.cloud = "A cloud reached the boss"
-	--L.cone = "\"Frontal\" cone" -- "Frontal" Cone, it's a rear cone (he's farting)
+	--L.aoe = "АоЕ урон ближнего боя"
+	--L.cloud = "Облако достигло босса"
+	--L.cone = "\"Frontal\" конус" -- "Frontal" Конус, конус сзади (он пукает)
 end
 
 L = BigWigs:NewBossLocale("Viscous Fallout Discovery", "ruRU")
 if L then
 	L.bossName = "Липкая муть"
-	--L.desiccated_fallout = "Desiccated Fallout" -- NPC ID 216810
+	--L.desiccated_fallout = "Ожог от радиации" -- NPC ID 216810
 end
 
 L = BigWigs:NewBossLocale("Crowd Pummeler 9-60 Discovery", "ruRU")
@@ -26,18 +26,18 @@ end
 L = BigWigs:NewBossLocale("Mechanical Menagerie Discovery", "ruRU")
 if L then
 	L.bossName = "Механический зверинец"
-	--L.attack_buff = "+50% attack speed"
-	--L.dont_attack = "Don't attack the sheep"
-	--L.sheep_safe = "Sheep is safe to attack"
+	--L.attack_buff = "+50% скорость атаки"
+	--L.dont_attack = "Не атакуйте овец"
+	--L.sheep_safe = "Овцу можно безопасно атаковать"
 
-	--L[218242] = "Dragon"
-	--L[218243] = "Sheep"
-	--L[218244] = "Squirrel"
-	--L[218245] = "Chicken"
+	--L[218242] = "Дракон"
+	--L[218243] = "Овца"
+	--L[218244] = "Белка"
+	--L[218245] = "Курица"
 end
 
 L = BigWigs:NewBossLocale("Mekgineer Thermaplugg Discovery", "ruRU")
 if L then
-	L.bossName = "Анжинер Термоштепсель"
-	--L.interruptable = "Interruptable"
+	L.bossName = "Мекжинер Термоштепсель"
+	--L.interruptable = "Прерываемый"
 end
